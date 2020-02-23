e8b0720923a21acf6cd15db15835f7fb941359f2
github username <github email address>
2020-02-22 19:11:45 -0500

    committing partner work

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,47 @@
 class Startup
+  
+  attr_accessor :name 
+  attr_reader   :founder, :domain
+  attr_writer 
+  @@all = []
+  @@alldomains = []
+
+  def initialize(founder)
+    @founder = founder 
+    @@all << self 
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+    @@alldomains << domain 
+  end
+  
+  def self.all 
+    @@all
+  end 
+
+  def self.find_by_founder(founder_name)
+    @@all.first { |startupinstance| startupinstance.founder == founder_name }
+  end 
+
+  def self.domains 
+    @@alldomains 
+  end
+
+
 
 end
+
+
+    
+   
+
+
+
+
+
+
+
+
+
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,33 @@
+require "pry"
+
 class VentureCapitalist
+attr_accessor :name 
+
+@@all = []
+
+def initialize
+  @@all << self 
+end 
+
+def total_worth #come back and add all investments (Int) 
+
+end
+
+def self.all
+  @@all
+end
+
+def self.tres_commas_club
+    billionaire = 
+    @@all.select { |vc| vc.total_worth > 1,000,000,000 }
+end
+
+morganandpierce = VentureCapitalist.new
+morganandpierce.name = "Morgan and Pierce"
+bensonandhedges = VentureCapitalist.new 
+bensonandhedges.name = "Benson and Hedges"
+
+binding.pry
+
 
 end
